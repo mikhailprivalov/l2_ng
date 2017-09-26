e87a7cf79f57dc51ba4bc874c4f6714ebe85b52a
Привалов М.С <n5250@ya.ru>
2017-09-26 13:35:30 +0800

    Добавлены мутации

--- resources/assets/js/src/store/modules/auth.js
+++ resources/assets/js/src/store/modules/auth.js
@@ -1,7 +1,23 @@
+import * as types from '../mutation-types'
+
 const state = {
   isLoggedIn: !!localStorage.getItem('token')
 }
 
+const mutations = {
+  [types.LOGIN] (state) {
+    state.pending = true
+  },
+  [types.LOGIN_SUCCESS] (state) {
+    state.isLoggedIn = true
+    state.pending = false
+  },
+  [types.LOGOUT] (state) {
+    state.isLoggedIn = false
+  }
+}
+
 export default {
-  state
+  state,
+  mutations
 }
