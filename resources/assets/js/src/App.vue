54a2276e4d8d629ea9eba5320178133ecd77f7cc
Привалов М.С <n5250@ya.ru>
2017-09-26 13:03:17 +0800

    Удаление лого, использование bootstrap

--- resources/assets/js/src/App.vue
+++ resources/assets/js/src/App.vue
@@ -1,8 +1,7 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <b-container fluid id="app">
     <router-view></router-view>
-  </div>
+  </b-container>
 </template>
 
 <script>
@@ -10,14 +9,3 @@
     name: 'app'
   }
 </script>
-
-<style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-</style>
