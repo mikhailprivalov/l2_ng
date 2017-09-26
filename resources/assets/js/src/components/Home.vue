e8448c84c17e22aed228c7cfc3598fbe07c82770
Привалов М.С <n5250@ya.ru>
2017-09-26 11:18:49 +0800

    Добавлен компонент Home

---
+++ resources/assets/js/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>Главная страница</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  mounted () {
+    console.log('mounted')
+  }
+}
+</script>
