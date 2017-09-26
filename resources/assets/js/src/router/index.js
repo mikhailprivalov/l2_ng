d767e5eacc5ef2c7d61d619252fff3238658ba4e
Привалов М.С <n5250@ya.ru>
2017-09-26 13:09:59 +0800

    Главная страница в /

--- resources/assets/js/src/router/index.js
+++ resources/assets/js/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 import Home from '@/components/Home'
 
 Vue.use(Router)
@@ -9,11 +8,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
-    },
-    {
-      path: '/home',
+      name: 'Home',
       component: Home
     }
   ]
